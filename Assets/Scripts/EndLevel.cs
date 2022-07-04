ac76c30712a55478332129a372bda6b94ea47819
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-07-04 14:33:03 +0500

    win/lose canvas

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class EndLevel : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _playerCamera;
     [SerializeField] private CinemachineVirtualCamera _endLEvelCamera;
-    [SerializeField] private GameObject _endLEvelPanel;
-    [SerializeField] private GameObject _progressSlider;
+    [SerializeField] private Canvas _winCanvas;
+    [SerializeField] private Canvas _loseCanvas;
+    [SerializeField] private Slider _progressSlider;
 
     private void Awake()
     {
+        _winCanvas.enabled = false;
+        _loseCanvas.enabled = false;
         _progressSlider.gameObject.SetActive(true);
-        _endLEvelPanel.gameObject.SetActive(false);
         _endLEvelCamera.gameObject.SetActive(false);
         _playerCamera.gameObject.SetActive(true);
     }
@@ -20,8 +23,8 @@ public class EndLevel : MonoBehaviour
     {
         if (Event.current.Equals(Event.KeyboardEvent(KeyCode.Space.ToString())))
         {
+            _winCanvas.enabled = true;
             _progressSlider.gameObject.SetActive(false);
-            _endLEvelPanel.gameObject.SetActive(true);
             _endLEvelCamera.gameObject.SetActive(true);
             _playerCamera.gameObject.SetActive(false);
         }
