400bdd12cdc0411a415fbb75296e19271ef2a8ca
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-07-07 16:08:20 +0500

    calculate accuracy percent script

---
+++ Assets/Scripts/Character/PlayerAccuracyCalculation.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerAccuracyCalculation : MonoBehaviour
+{
+    [SerializeField] private LevelProgress _levelProgress;
+    [SerializeField] private EndLevelTrigger _endLevelTrigger;
+    [SerializeField] private TMP_Text _percentText;
+
+    private string _accuracyPercent;
+
+    private void OnEnable()
+    {
+        _endLevelTrigger.Won += CheckPercentAccuracy;
+    }
+
+    private void OnDisable()
+    {
+        _endLevelTrigger.Won -= CheckPercentAccuracy;
+    }
+
+    private void CheckPercentAccuracy()
+    {
+        _accuracyPercent = (1f - _levelProgress.LosePercentage).ToString("0.00") + "% Accuracy";
+
+        _percentText.text = _accuracyPercent;
+    }
+}
