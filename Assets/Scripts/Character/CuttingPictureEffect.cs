d7942972ae7732a146f4a3eb42b74e5e8fe59827
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-07-08 20:42:19 +0500

    change player color fix

--- Assets/Scripts/Character/CuttingPictureEffect.cs
+++ Assets/Scripts/Character/CuttingPictureEffect.cs
@@ -6,8 +6,12 @@ public class CuttingPictureEffect : MonoBehaviour
     [SerializeField] private PlayerMovement _playerMovement;
     [SerializeField] private Transform _model;
     [SerializeField] private PictureEvents _puctureEvents;
-    [SerializeField] private ParticleSystem _cuttingPicturetemplate;
-    [SerializeField] private Transform _effectContainer;
+    [SerializeField] private ParticleSystem _smokeEffect;
+    [SerializeField] private Transform _smokeEffectContainer;
+    [SerializeField] private Material _playerMaterial;
+    [SerializeField] private Color _targetColor;
+
+    private Color _startColor;
 
     private void OnEnable()
     {
@@ -19,12 +23,25 @@ public class CuttingPictureEffect : MonoBehaviour
         _puctureEvents.CuttingPicture -= OnCutPicture;
     }
 
+    private void Start()
+    {
+        _startColor = new Color(1, 1, 1);
+
+        _playerMaterial.color = _startColor;
+    }
+
     private void OnCutPicture()
     {
         var duration = 0.05f;
         _model.DOShakeScale(duration, 0.25f, 10);
-        
+        ChangeColor(_targetColor, 2, _startColor, 2);
         _playerMovement.ChangeSpeed(0.5f, duration);
-        Instantiate(_cuttingPicturetemplate, _effectContainer);
+        Instantiate(_smokeEffect, _smokeEffectContainer);
+    }
+
+    private void ChangeColor(Color targetColor, float durationToTarget, Color normalColor, float durationToNormal)
+    {
+        _playerMaterial.DOColor(targetColor, durationToTarget).
+            OnComplete(() => _playerMaterial.DOColor(normalColor, durationToNormal));
     }
 }
--- Assets/Scripts/Character/PictureEvents.cs
+++ Assets/Scripts/Character/PictureEvents.cs
@@ -5,12 +5,10 @@ using DG.Tweening;
 public class PictureEvents : MonoBehaviour
 {
     [SerializeField] private LevelProgress _progress;
-    [SerializeField] private Material _lawnMoverMaterial;
 
     private float _previousLoseProgress;
     private float _previousWinProgress;
-    private Color _normarColor;
-    private Color _targetColor;
+
 
     public event UnityAction CuttingPicture;
     public event UnityAction CuttingGrass;
@@ -29,9 +27,6 @@ public class PictureEvents : MonoBehaviour
     {
         _previousLoseProgress = 0f;
         _previousWinProgress = 0f;
-
-        _normarColor = new Color(1, 1, 1);
-        _targetColor = new Color(0.3396226f, 0.3396226f, 0.3396226f);
     }
 
     private void OnUpdateProgress()
@@ -39,19 +34,12 @@ public class PictureEvents : MonoBehaviour
         if (_progress.LosePercentage > _previousLoseProgress)
         {
             _previousLoseProgress = _progress.LosePercentage;
-            ChangeColor(_targetColor, 3);
             CuttingPicture?.Invoke();
         }
         if (_progress.WinPercentage > _previousWinProgress)
         {
             _previousWinProgress = _progress.WinPercentage;
-            ChangeColor(_normarColor, 1);
             CuttingGrass?.Invoke();
         }
-    }
-
-    private void ChangeColor(Color targetColor, float duration)
-    {
-        _lawnMoverMaterial.DOColor(targetColor, duration);
     }
 }
