1497ddd9fd4816b2c87c98aa836534f5abcb8236
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-07-14 12:44:29 +0500

    refactoring

--- Assets/Scripts/Character/CuttingGrassEffect.cs
+++ Assets/Scripts/Character/CuttingGrassEffect.cs
@@ -5,7 +5,6 @@ public class CuttingGrassEffect : MonoBehaviour
     [SerializeField] private PictureEvents _levelEvents;
     [SerializeField] private ParticleSystem _cuttingEffectTemplate;
     [SerializeField] private Transform _effectContainer;
-    [SerializeField] private Animator _sawAnimator;
 
     private void OnEnable()
     {
@@ -15,12 +14,10 @@ public class CuttingGrassEffect : MonoBehaviour
     private void OnDisable()
     {
         _levelEvents.CuttingGrass -= OnCuttingGrass;
-        _sawAnimator.speed = 1;
     }
 
     private void OnCuttingGrass()
     {
         Instantiate(_cuttingEffectTemplate, _effectContainer);
-        _sawAnimator.speed = 2;
     }
 }
