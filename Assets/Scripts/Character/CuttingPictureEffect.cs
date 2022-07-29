eaf619d63b50d2a0988cc0cea35afda6568e64b2
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-07-29 12:58:40 +0500

    change grass picture effect

--- Assets/Scripts/Character/CuttingPictureEffect.cs
+++ Assets/Scripts/Character/CuttingPictureEffect.cs
@@ -1,15 +1,10 @@
 using UnityEngine;
-using DG.Tweening;
 
 public class CuttingPictureEffect : MonoBehaviour
 {
     [SerializeField] private PictureEvents _puctureEvents;
     [SerializeField] private PlayerMovement _playerMovement;
-    [SerializeField] private Transform _model;
-    [SerializeField] private Transform _smokeEffectContainer;
-    [SerializeField] private ParticleSystem _smokeEffect;
     [SerializeField] private Renderer _renderer;
-    [SerializeField] private Color _targetColor;
 
     private Color _startColor;
     private Material _material;
@@ -34,20 +29,6 @@ public class CuttingPictureEffect : MonoBehaviour
     private void OnCutPicture()
     {
         var duration = 0.05f;
-
-        _model.transform.localScale = Vector3.one;
-        _model.DOShakeScale(duration, 0.25f, 10);
         _playerMovement.ChangeSpeed(0.2f, duration);
-
-        ChangeColor(_targetColor, duration * 10, _startColor, duration * 10);
-        var inst = Instantiate(_smokeEffect, _smokeEffectContainer);
-        inst.transform.localPosition = Vector3.zero;
-    }
-
-    private void ChangeColor(Color targetColor, float durationToTarget, Color normalColor, float durationToNormal)
-    {
-        _material.DOKill();
-        _material.DOColor(targetColor, durationToTarget)
-            .OnComplete(() => _material.DOColor(normalColor, durationToNormal));
     }
 }
